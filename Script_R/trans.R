74cc5a1bbac4c4b3b693e540eb00b4bcdc42f36a
cms0416 <cms0416@naver.com>
2024-11-04 18:01:46 +0900

    -

--- Script_R/trans.R
+++ Script_R/trans.R
@@ -7,6 +7,11 @@ library(janitor)
 ##########  파일 불러오기  #####################################################
 trans <- read_excel("trans.xlsx")
 
+split <- read_excel("trans.xlsx", sheet = 2)
+
+sec <- read_excel("trans.xlsx", sheet = 3)
 
 
-
+##########  데이터 정리  #######################################################
+trans_1 <- trans %>% 
+  left_join(sec, join_by(거래일자 >= 날짜))
